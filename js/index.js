0bf64243b9fe3b4cd26b7dfabe9ebdefa44fc634
ZoitovNurillo <nurillozoitov97@gmail.com>
2024-09-26 17:26:07 +0500

    Add close btn and open btn script

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const modifiers = {
+  elHeaderOpenEls: 'header__toggler--open'
+}
+
+let toggle = true;
+
+const elBtnOpen = document.querySelector(".btn__toggler--js");
+const elBtnClose = document.querySelector(".nav__close--js");
+const elHeader = document.querySelector(".header")
+
+  elBtnOpen.addEventListener("click", () => {
+    elHeader.classList.add(modifiers.elHeaderOpenEls);
+  })
+  elBtnClose.addEventListener("click", () => {
+        elHeader.classList.remove(modifiers.elHeaderOpenEls);
+
+      })
