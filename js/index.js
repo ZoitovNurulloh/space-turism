de5243ce74fa813e1908655c8ae73a1226180896
ZoitovNurillo <nurillozoitov97@gmail.com>
2024-10-01 15:45:31 +0500

    Add destination menu script

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@ const modifiers = {
   elHeaderOpenEls: 'header__toggler--open'
 }
 
-let toggle = true;
-
 const elBtnOpen = document.querySelector(".btn__toggler--js");
 const elBtnClose = document.querySelector(".nav__close--js");
 const elHeader = document.querySelector(".header")
@@ -13,4 +11,52 @@ elBtnOpen.addEventListener("click", () => {
 })
 elBtnClose.addEventListener("click", () => {
   elHeader.classList.remove(modifiers.elHeaderOpenEls);
+})
+
+const destinationBtn = document.querySelectorAll(".btn__destination--js");
+const btnInfo = document.getElementById("destination-moon");
+const btnName = document.getElementById("destination__name");
+const btnImg = document.getElementById("destination__img")
+const btnDistance = document.getElementById("destiantion__distance");
+const btnTravel = document.getElementById("destination__travel");
+
+destinationBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    destinationBtn.forEach(el => el.classList.remove('link__active'));
+
+    btn.classList.add('link__active')
+
+    const getData = (resourse) => {
+      return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener("readystatechange", () => {
+          if(request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            resolve(data);
+          }else if(request.readyState === 4) {
+            reject('error');
+          }
+        })
+
+
+        request.open("GET", resourse);
+        request.send();
+      })
+    }
+
+    getData("../json/data.json").then((data) => {
+      data.destinations.forEach((el, index)=> {
+        if(btn.textContent == el.name) {
+          btnName.textContent = el.name;
+          btnInfo.textContent = el.description;
+          btnImg.setAttribute("src", el.images.png);
+          btnDistance.textContent = el.distance;
+          btnTravel.textContent = el.travel;
+        }
+      })
+    }).catch((err) => {
+      console.log(err);
+    })
+  })
 })
